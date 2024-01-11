485f2b01fc15fded1d16e0aea46a3cf4b7e95a07
Melodi Balossino <melodibalossino@gmail.com>
2024-01-11 18:07:14 -0300

    Services

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,38 +1,92 @@
 import React from 'react';
 import styles from './Services.module.css';
 import { BsArrowDownShort } from "react-icons/bs";
-import services from './services.js';
+
+import { BsDisplay, BsDatabase, BsClockHistory, BsLink, BsFillPersonCheckFill,  } from "react-icons/bs";
+import { AiOutlineTeam } from "react-icons/ai";
 
 function Services () {
+  const oQueFazemos = [ 
+    {
+      icon: <BsDisplay />,
+      title: 'Soluções Digitais',
+      description: 'Produtos digitais mobile, Web ou novos serviços.'
+    },
+    {
+      icon: <BsDatabase />,
+      title: 'Dados, IA Machine Learning',
+      description: 'Produtos de dados alinhados aos seus objetivos de growth.'
+    },
+    {
+      icon: <BsClockHistory />,
+      title: 'Transformação Ágil',
+      description: 'Agilidade do nível estratégico ao operacional.'
+    },
+    {
+      icon: <BsLink />,
+      title: 'Plataformas e Alianças Tecnológicas',
+      description: 'Integrações estratégicas que potencializam seus resultados.'
+    }
+  ]
+
+  const comoFazemos = [
+    {
+      icon: <AiOutlineTeam />,
+      title: 'Squads Dedicadas Gerenciadas',
+      description: 'Equipes de trabalho conectadas para resolver os objetivos do negócio.'
+    },
+    {
+      icon: <BsFillPersonCheckFill />,
+      title: 'Smartsourcing',
+      description: 'Alocação de pessoas altamente capacitadas com um modelo de funcionamento único no mercado.'
+    },
+    {
+      icon: <BsLink />,
+      title: 'Vem Ser Trilogy',
+      description: 'Programa de aceleração de conhecimento técnico, capacitando profissionais que podem ser alocados em sua empresa.'
+    }
+  ]
 
   return (
-    <div className={styles.container}>
-        <div className={styles.subtitles}>
-          <div className={styles.subtitleBlock}>
-            <span className={styles.subtitle}>O QUE FAZEMOS</span>
-            <BsArrowDownShort className={styles.iconChevron} size={25} />
-            <hr className={styles.line} />
-          </div>
-          <div className={styles.subtitleBlock}>
-            <span className={styles.subtitle}>COMO FAZEMOS</span>
-            <BsArrowDownShort className={styles.iconChevron} size={25} />
-            <hr className={styles.line} />
-          </div>
+     <div className={styles.container}>
+      <div className={styles.section}>
+        <div className={styles.header}>
+           <BsArrowDownShort className={styles.arrowIcon} />
+          <h2>O QUE FAZEMOS</h2>
         </div>
-      <div className={styles.content}>
-        {services.map((service, index) => (
-          <div className={styles.contentBlock} key={index}>
-            <div className={styles.icon}>{/* Aquí iría el componente del icono o la etiqueta img */}</div>
-            <h3 className={styles.contentTitle}>{service.title}</h3>
-            <p className={styles.contentDescription}>{service.description}</p>
+        <hr className={styles.underline} />
+        {oQueFazemos.map((service, index) => (
+          <div className={styles.service} key={index}>
+            <div className={styles.containerIcon}>
+            <div className={styles.iconOQueFazemos}>{service.icon}</div>
+            </div>
+            <div>
+              <h3>{service.title}</h3>
+              <p>{service.description}</p>
+            </div>
           </div>
         ))}
       </div>
-      <div className={styles.footer}>
-        <span className={styles.hashtag}>#TECH UP TOGETHER #TECH</span>
+      <div className={styles.section}>
+        <div className={styles.header}>
+          <BsArrowDownShort className={styles.arrowIcon} />
+          <h2>COMO FAZEMOS</h2>
+        </div>
+        <hr className={styles.underline} />
+        {comoFazemos.map((service, index) => (
+          <div className={styles.service} key={index}>
+            <div className={styles.containerIcon}>
+            <div className={styles.iconComoFazemos}>{service.icon}</div>
+            </div>
+            <div>
+              <h3>{service.title}</h3>
+              <p>{service.description}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
-};
+}
 
 export default Services;

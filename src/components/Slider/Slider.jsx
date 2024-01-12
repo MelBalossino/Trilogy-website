a2029dff605f63c3ec42b0a25ed32489262dc55b
Melodi Balossino <melodibalossino@gmail.com>
2024-01-12 15:10:28 -0300

    Slider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,10 +1,39 @@
+import React from 'react';
+import styles from './Slider.module.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import { Keyboard, Autoplay } from 'swiper/modules';
 
-function Slider(){
+
+const Slider = () => {
     return (
-        <div className="slider">
-            <h2>Slider</h2>
-        </div>
+                <Swiper
+                    slidesPerView={1}
+                    spaceBetween={30}
+                    keyboard={{
+                        enabled: true,
+                    }}
+                    autoplay={{
+                        delay: 3000,
+                        disableOnInteraction: false
+                    }}
+                    loop={true}
+                    modules={[Keyboard, Autoplay]}
+                    className={styles.mySlider}
+                >
+                    <SwiperSlide >
+                            <img src="./images/banner-2.png" />
+                    </SwiperSlide>
+
+                    <SwiperSlide >
+                            <img src="./images/banner-2.png" />
+                    </SwiperSlide>
+                    <section className={styles.titleSlider}>
+                <h1> Trilogy <br /> Solutions </h1>
+                <p>Desenvolvemos softwares e soluções de integração <br /> com entregas de alto padrão para sua empresa!</p>
+            </section>
+                </Swiper>
     )
 }
 
-export default Slider
+export default Slider;

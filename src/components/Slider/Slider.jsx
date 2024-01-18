28e4a8b8a430ea8a0aa562fe848fc407cec993d8
Melodi Balossino <melodibalossino@gmail.com>
2024-01-18 13:07:10 -0300

    Fix-details

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -8,42 +8,39 @@ import { BsArrowRight } from 'react-icons/bs'
 
 
 const Slider = () => {
-
     return (
-                <Swiper
-                    slidesPerView={1}
-                    spaceBetween={30}
-                    keyboard={{
-                        enabled: true,
-                    }}
-                    autoplay={{
-                        delay: 4000,
-                        disableOnInteraction: false
-                    }}
-                    loop={true}
-                    modules={[Keyboard, Autoplay]}
-                    className={styles.mySlider}
-                    id='slider'
-                >
-                    <SwiperSlide >
-                             <div className={styles.fondoSlider} >
-                             
-                             
-      <div className={styles.franjaAzul} ></div>
-      </div>
-                    </SwiperSlide>
+        <Swiper
+            slidesPerView={1}
+            spaceBetween={30}
+            keyboard={{
+                enabled: true,
+            }}
+            autoplay={{
+                delay: 4000,
+                disableOnInteraction: false
+            }}
+            loop={true}
+            modules={[Keyboard, Autoplay]}
+            className={styles.mySlider}
+            id='slider'
+        >
+            <SwiperSlide >
+                <div className={styles.fondoSlider} >
+                    <div className={styles.franjaAzul} ></div>
+                </div>
+            </SwiperSlide>
 
-                    <SwiperSlide >
-                            <img src="./images/banner-2.png" />
-                    </SwiperSlide>
-                    <section className={styles.titleSlider}>
+            <SwiperSlide >
+                <img src="./images/banner-2.png" alt='People working in an office'/>
+            </SwiperSlide>
+            <section className={styles.titleSlider}>
                 <h1> Trilogy <br /> Solutions </h1>
                 <p>Desenvolvemos softwares e soluções de integração <br /> com entregas de alto padrão para sua empresa!</p>
                 <Link to="sobre" spy={true} smooth={true} offset={-70} duration={500}>
-                <button>Conheça a Trilogy <BsArrowRight className={styles.iconChevron} size={25} /></button>
+                    <button>Conheça a Trilogy <BsArrowRight className={styles.iconChevron} size={25} /></button>
                 </Link>
             </section>
-                </Swiper>
+        </Swiper>
     )
 }
 
